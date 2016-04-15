03280311f82a7f9db46728094d2f985a5dd6c8ec
Admin <olegtranslator@gmail.com>
2016-04-15 12:46:18 +0200

    first commit

---
+++ data/login.js
@@ -0,0 +1,9 @@
+module.exports = {
+    username: '',
+    password: '',
+    testEmail: 'olegtranslator@gmail.com',
+    firstName: 'Anna',
+    lastName: 'Wolf',
+    testFirstName: 'Name',
+    testLastName: 'Last'
+};
